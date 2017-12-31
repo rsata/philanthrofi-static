5257aec551be51eb45c79f13bd2a5ab37cd324b5
Reid <reid.t.sata@gmail.com>
2017-12-31 13:28:49 -0800

    test form

--- static/main.js
+++ static/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
+  var $form = $('form#join-website-form')
+  var url = 'https://script.google.com/macros/u/0/s/AKfycbyCXLu__s7aggSvvqghSlKF7T06cO2lfG5i5RDyALiwWa8iAuId/exec'
 
+  $('#submit-join-website-form').on('click', function(e) {
+    e.preventDefault();
+    // console.log($form.serializeObject());
+    var jqxhr = $.ajax({
+      url: url,
+      method: "GET",
+      dataType: "json",
+      data: $form.serializeObject()
+    })
+    // .success(
+    //   //
+    // );
+  })
 
 })
